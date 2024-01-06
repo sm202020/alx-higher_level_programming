fa5c81e625cba5d3e06b7563b0649c3eb6bc96f4
Sunday Akinbode Mebawondu <akinbode4life@gmail.com>
2024-01-06 12:24:49 -0800

    0x03

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,6 @@
-p#!/usr/bin/python3
+#!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        first_char = None
-    else:
-        first_char = sentence[0]
-
-    result_tuple = (len(sentence), first_char)
-    return result_tuple
+    length = len(sentence)
+    first_char = sentence[0] if length > 0 else "None"
+    tup = length, first_char
+    return(tup)
