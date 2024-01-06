7e6dd6a0399fe255fab7a02682c4b2856221705b
Sunday Akinbode Mebawondu <akinbode4life@gmail.com>
2024-01-06 11:43:11 -0800

    0x03

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
+
 def no_c(my_string):
-    new_str = ""
-    for i in my_string:
-        if i is not 'c' and i is not 'C':
-            new_str += i
-    return (new_str)
-# return my_string.translate({ord(c): None for c in "cC"})
+
+    copy = [a for a in my_string if a != 'c' and a != 'C']
+    return ("".join(copy))
