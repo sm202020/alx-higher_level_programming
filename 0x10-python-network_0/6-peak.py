23ae59e2cdb392b31cbc2715bb4e73e037ed3060
Sunday Mebawondu <akinbode4life.com>
2024-04-27 05:48:20 -0700

    update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python3
-"""Algorithms for list of integers."""
+"""
+   this function finds a peak in a list of unsorted integers
+"""
 
 
-def find_peak(list_of_integers):
-    """Finds a peak in a list of unsorted integers."""
-    if list_of_integers:
-        list_of_integers.sort(reverse=True)
-        return list_of_integers[0]
+def find_peak(numbr):
+    '''
+        Finds the peak in a list of numbers
+    '''
+    length = len(numbr)
+    if length == 0:
+        return None
+    if length == 1:
+        return (numbr[0])
+    if length == 2:
+        return numbr[0] if numbr[0] >= numbr[1] else numbr[1]
+
+    for idx in range(0, length):
+        value = numbr[idx]
+        if (idx > 0 and idx < length - 1 and
+                numbr[idx + 1] <= value and numbr[idx - 1] <= value):
+                return value
+        elif idx == 0 and numbr[idx + 1] <= value:
+            return value
+        elif idx == length - 1 and numbr[idx - 1] <= value:
+            return value
+    return pick
